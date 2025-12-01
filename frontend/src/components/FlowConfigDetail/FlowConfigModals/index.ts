032d7357c5f596d402c4305312dae2b0da97c0d1
Sebastian Russo <russo.sebastian@gmail.com>
2025-12-01 10:11:37 -0500

    feat: Added  modal that lets admins define the allowed options for a Select-type variable.

--- frontend/src/components/FlowConfigDetail/FlowConfigModals/index.ts
+++ frontend/src/components/FlowConfigDetail/FlowConfigModals/index.ts
@@ -1,3 +1,4 @@
 export { AddVariableModal } from './AddVariableModal';
 export { AddPromptModal } from './AddPromptModal';
 export { AddLocaleModal } from './AddLocaleModal';
+export { SelectOptionsModal } from './SelectOptionsModal';
